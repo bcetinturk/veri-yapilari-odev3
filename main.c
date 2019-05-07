adbb47271b00f3e143d3c518118c0f13469ba36c
bariscetinturk <baris48cetinturk@gmail.com>
2019-05-07 17:56:57 +0300

    added randomness for key selection

--- main.c
+++ main.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void printArrays(int arr[], int n){
+    for(int i=0; i<n; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
 
 int partition(int arr[], int low, int high, int pivot){
     int i = low;
@@ -23,36 +32,33 @@ int partition(int arr[], int low, int high, int pivot){
     return i;
 }
 
-void matchPairs(int keys[], int locks[], int low, int high){
+void matchKeysAndLocks(int keys[], int locks[], int low, int high){
     if (low < high){
-        int pivot = partition(keys, low, high, locks[high]);
+        int index = rand() % (high-low+1) + low;
+        printf("random index: %d\n", index);
+        int pivot = partition(locks, low, high, keys[index]);
 
-        partition(locks, low, high, keys[pivot]);
+        partition(keys, low, high, locks[pivot]);
 
-        matchPairs(keys, locks, low, pivot-1);
-        matchPairs(keys, locks, pivot+1, high);
-    }
-}
-
-void printArrays(int keys[], int locks[], int n){
-    printf("Keys: ");
-    for(int i=0; i<n; i++){
-        printf("%d ", keys[i]);
-    }
-    printf("locks: ");
-    for(int i=0; i<n; i++){
-        printf("%d ", locks[i]);
+        matchKeysAndLocks(keys, locks, low, pivot-1);
+        matchKeysAndLocks(keys, locks, pivot+1, high);
     }
 }
 
 int main(){
-    int keys[10] = {2,5,6,8,7,3,6,1,4,0};
-    int locks[10] = {6,8,2,3,9,4,1,5,7,0};
+    srand(time(NULL));
+    int keys[20] = {2,6,8,5,1,9,7,3,4,0,15,19,18,14,12,11,13,17,16,10};
+    int locks[20] = {9,2,3,4,6,1,5,7,0,8,10,14,16,11,15,19,17,13,12,18};
 
     int n = sizeof(keys)/ sizeof(int);
 
-    matchPairs(keys, locks, 0, n);
-    printArrays(keys, locks, n);
+    printArrays(keys, n);
+    printArrays(locks, n);
+
+    matchKeysAndLocks(keys, locks, 0, n-1);
+
+    printArrays(keys, n);
+    printArrays(locks, n);
 
     return 0;
 }
